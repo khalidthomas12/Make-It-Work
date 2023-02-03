7a6e3ae687e07b40bd4711a602e3560a56c56282
kT <khalid.thomas12@gmail.com>
2023-02-02 19:16:54 -0500

    Initial Commit

---
+++ Make It Work/Make_It_WorkApp.swift
@@ -0,0 +1,17 @@
+//
+//  Make_It_WorkApp.swift
+//  Make It Work
+//
+//  Created by Khalid Thomas on 2/2/23.
+//
+
+import SwiftUI
+
+@main
+struct Make_It_WorkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
